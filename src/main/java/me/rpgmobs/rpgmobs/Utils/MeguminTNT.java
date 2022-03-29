bc3aec3a79154f1c28f0d8c5d0633774db38e25a
Kitoamv <richard.barbosa078@gmail.com>
2022-03-29 00:32:26 -0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/me/rpgmobs/rpgmobs/Utils/MeguminTNT.java
+++ src/main/java/me/rpgmobs/rpgmobs/Utils/MeguminTNT.java
@@ -21,17 +21,18 @@ public class MeguminTNT implements Listener {
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-            if(Cooldown.checkCooldown(event.getPlayer()))
-            if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta() != null && event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore() != null
-            && event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore().contains(ChatColor.GRAY + "- Arremesa uma magia explosiva capaz de explodir os inimigos")){
-                FallingBlock tnt = event.getPlayer().getWorld().spawnFallingBlock(event.getPlayer().getEyeLocation(), Material.TNT, (byte) 0);
-                tnt.setDropItem(false);
-                throwedtnt.add(tnt);
-                tnt.setVelocity(event.getPlayer().getLocation().getDirection().multiply(3));
-                event.setCancelled(true);
-                Cooldown.setCooldown(event.getPlayer(), 5);
-            } else{
-                player.sendMessage(ChatColor.DARK_RED + "A Habilidade ainda não está preparada.");
+            if (Cooldown.checkCooldown(event.getPlayer())) {
+                if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta() != null && event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore() != null
+                        && event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore().contains(ChatColor.GRAY + "- Arremesa uma magia explosiva capaz de explodir os inimigos")) {
+                    FallingBlock tnt = event.getPlayer().getWorld().spawnFallingBlock(event.getPlayer().getEyeLocation(), Material.TNT, (byte) 0);
+                    tnt.setDropItem(false);
+                    throwedtnt.add(tnt);
+                    tnt.setVelocity(event.getPlayer().getLocation().getDirection().multiply(3));
+                    event.setCancelled(true);
+                    Cooldown.setCooldown(event.getPlayer(), 5);
+                } else {
+                    player.sendMessage(ChatColor.DARK_RED + "A Habilidade ainda não está preparada.");
+                }
             }
         }
     }
@@ -40,7 +41,7 @@ public class MeguminTNT implements Listener {
     public void onFall(EntityChangeBlockEvent event){
         if(event.getEntity() instanceof  FallingBlock){
             if(throwedtnt.contains(event.getEntity())){
-                event.getEntity().getWorld().createExplosion(event.getEntity().getLocation(), 30, false, false);
+                event.getEntity().getWorld().createExplosion(event.getEntity().getLocation(), 15, false, false);
                 event.setCancelled(true);
                 event.getEntity().remove();;
                 throwedtnt.remove(event.getEntity());
